de5aeb24134410dceb5f8a25055c0592d63719bc
kailicen <kailicen226@gmail.com>
2023-04-30 14:58:57 +1000

    k

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,14 +11,14 @@ function Hero({}: Props) {
     >
       <div className="flex flex-col items-center justify-center w-full flex-1 px-3 md:px-32 lg:px-52 text-center">
         <h1 className="text-2xl md:text-5xl md:leading-[65px] font-bold">
-          Kaikul - A supportive self-help community where{" "}
+          KaiKul - A supportive self-help community where{" "}
           <span className="underline decoration-violet-500">
             sharing and growth
           </span>{" "}
           go hand in hand.
         </h1>
         <h2 className="mt-6 text-md md:text-lg px-0 md:px-32">
-          At Kaikul, we believe that personal growth thrives through connecting
+          At KaiKul, we believe that personal growth thrives through connecting
           with others, embracing diversity, and broadening perspectives. Join
           our self-help community to start your journey towards growth and
           inclusiveness today.
