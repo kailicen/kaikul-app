1c587cb195fd0d643d28f959cb961aa0ae675591
kailicen <kailicen226@gmail.com>
2023-05-13 12:59:24 +1000

    hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,13 +11,16 @@ function Hero({}: Props) {
     >
       <div className="flex flex-col items-center justify-center w-full flex-1 px-3 md:px-32 lg:px-52 text-center">
         <h1 className="text-2xl md:text-5xl md:leading-[65px] font-bold">
-          KaiKul: Track your goals with an accountable buddy.
+          KaiKul: Achieve Your Goals with an Accountability Partner
         </h1>
         <h2 className="mt-6 text-md md:text-lg px-0 md:px-32">
-          KaiKul is an AI-driven platform designed to facilitate goal tracking
-          by connecting individuals with shared personal objectives. The
-          platform emphasizes the value of meaningful connections and diversity
-          in pursuit of personal growth.
+          KaiKul is a goal tracking platform that connects you with an
+          accountability partner based on your goals and preferences. You
+          receive weekly updates, personal development material, and
+          notifications to help you stay on track. You collaborate with your
+          partner to achieve your goals and catch up weekly via a virtual or
+          in-person call. KaiKul provides personalized support and motivation to
+          help you reach your full potential.
         </h2>
         {/* <Waitlist /> */}
       </div>
