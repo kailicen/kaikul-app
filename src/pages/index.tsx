38c5b88b065be7ce06d551d24a2ec4cf2f04f8a4
kailicen <kailicen226@gmail.com>
2023-05-13 18:23:32 +1000

    navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,9 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Team from "@/components/Team";
 import Testimonial from "@/components/Testimonial";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 import Head from "next/head";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { SocialIcon } from "react-social-icons";
 
 type SectionRef = HTMLDivElement | null;
@@ -16,6 +17,12 @@ export default function Home() {
   const betaRef = useRef<SectionRef>(null);
   const teamRef = useRef<SectionRef>(null);
   const contactRef = useRef<SectionRef>(null);
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -75,7 +82,7 @@ export default function Home() {
         >
           KaiKul
         </div>
-        <div className="flex flex-row items-center space-x-2 md:space-x-5">
+        <div className="hidden md:flex md:flex-row items-center space-x-2 md:space-x-5">
           <div
             className="hover:text-violet-500 hover:cursor-pointer"
             onClick={scrollToFeatures}
@@ -104,6 +111,59 @@ export default function Home() {
             Contact
           </button>
         </div>
+        <Bars3Icon
+          onClick={toggleMenu}
+          className="block md:hidden h-8 w-8 text-black cursor-pointer"
+        />
+        {isMenuOpen && (
+          <div className="absolute top-20 right-0 w-full p-4 bg-white z-20 flex flex-col items-end">
+            <div
+              className="block py-2 px-4 hover:text-violet-500 hover:cursor-pointer"
+              onClick={() => {
+                toggleMenu();
+                scrollToFeatures();
+              }}
+            >
+              How It Works
+            </div>
+            <div
+              className="block py-2 px-4 hover:text-violet-500 hover:cursor-pointer"
+              onClick={() => {
+                toggleMenu();
+                scrollToTestimonial();
+              }}
+            >
+              Testimonial
+            </div>
+            <div
+              className="block py-2 px-4 hover:text-violet-500 hover:cursor-pointer"
+              onClick={() => {
+                toggleMenu();
+                scrollToBeta();
+              }}
+            >
+              Beta v1.1
+            </div>
+            <div
+              className="block py-2 px-4 hover:text-violet-500 hover:cursor-pointer"
+              onClick={() => {
+                toggleMenu();
+                scrollToTeam();
+              }}
+            >
+              Our Team
+            </div>
+            <button
+              className="block my-2 mx-4 buttonMobile md:button"
+              onClick={() => {
+                toggleMenu();
+                scrollToContact();
+              }}
+            >
+              Contact
+            </button>
+          </div>
+        )}
       </header>
 
       <div className="flex flex-col space-y-5">
