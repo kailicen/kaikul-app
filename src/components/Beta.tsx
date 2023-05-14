5b286357fafa31445d57864c228b5720ccdd8b31
kailicen <kailicen226@gmail.com>
2023-05-14 14:25:44 +1000

    form

--- src/components/Beta.tsx
+++ src/components/Beta.tsx
@@ -84,7 +84,7 @@ function Features({}: Props) {
       </div>
 
       <button className="buttonMobile md:button mt-5 md:mt-10 bg-gray-800 md:bg-gray-800">
-        <Link href="https://airtable.com/shrRsvclV6Q7vIppw/" target="_blank">
+        <Link href="https://airtable.com/shrRTSGtb5taQ50yO/" target="_blank">
           Join Beta Now
         </Link>
       </button>
