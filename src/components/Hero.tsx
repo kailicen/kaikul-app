b79b2379e5da21c38f9ccf8fdc55a5c6dd697d17
kailicen <kailicen226@gmail.com>
2023-05-21 10:36:51 +1000

    minor

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,7 +29,10 @@ function Hero({}: Props) {
         className="flex flex-col items-center md:items-end justify-center 
       w-full md:px-10 lg:px-20 xl:px-30 2xl:px-40 flex-1 px-3 text-center md:text-end"
       >
-        <h1 className="text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl font-bold max-w-[800px] text-violet-800">
+        <h1
+          className="text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl font-bold lg:max-w-7xl
+        max-w-[800px] text-violet-800"
+        >
           Self-Improvement. Weekly Changes. Companionship.
         </h1>
         <h2 className="mt-6 text-md md:text-lg lg:text-xl 2xl:text-2xl px-0 md:text-end max-w-[800px]">
