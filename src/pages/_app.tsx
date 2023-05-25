6dd0b080ccc32985ffbc62a78c1ed91e3717e9ef
kailicen <kailicen226@gmail.com>
2023-05-25 21:54:56 +1000

    hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import Hotjar from "@hotjar/browser";
+
+const siteId = 3505619;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
