b67857a0b4ae6e664f7533e0cce5abc69e32d0d6
kailicen <kailicen226@gmail.com>
2023-06-05 21:51:21 +1000

    cta, qna change

--- src/components/Qna.tsx
+++ src/components/Qna.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
 import React, { useState } from "react";
+import va from "@vercel/analytics";
 
 const questions = [
   {
     id: 1,
     question: "Who is KaiKul for?",
     answer:
-      "KaiKul is for anyone who wants to constantly improve themselves in a REAL, result-driven manner. ",
+      "KaiKul is for anyone who wants to constantly improve themselves in practical and impactful ways. Whether you aim to meditate regularly, read more, improve productivity, prioritize effectively, or make healthier choices, KaiKul provides the community, resources, and accountability to help you achieve real, meaningful results.",
   },
   {
     id: 2,
@@ -29,7 +31,7 @@ const questions = [
     id: 5,
     question: "Is KaiKul free?",
     answer:
-      "Yes! By joining our beta, you are helping us shape our app. We are committed to providing the best possible service for our users, and your feedback and participation play a crucial role in achieving that goal. Thank you for being a part of our journey! ",
+      "Absolutely! But act fast, as we have a limited beta program with only 100 spots available until June 2023. ",
   },
 ];
 
@@ -52,7 +54,7 @@ function Qna({}: Props) {
       <h3 className="mb-10 text-3xl font-bold text-violet-800">
         Frequently Asked Questions
       </h3>
-      <div className="space-y-4 w-full md:w-[80%] lg:w-[60%]">
+      <div className="space-y-4 w-full md:w-[80%] lg:w-[60%] mb-10">
         {questions.map((question) => (
           <div
             key={question.id}
@@ -85,6 +87,32 @@ function Qna({}: Props) {
             )}
           </div>
         ))}
+      </div>
+      <div className="bg-violet-200 w-screen py-5 flex flex-col items-center">
+        <div className="text-xl font-bold text-violet-800 mb-5">
+          Don&apos;t hesitate - Schedule an onboarding call with us at no cost!
+          🎉🔥
+        </div>
+        <div className="flex flex-col md:flex-row md:space-x-4">
+          <button className="buttonMobile md:button mt-2 md:mt-3 text-md md:text-xl 2xl:mt-16">
+            <Link
+              href="https://calendly.com/setthawut-kul/onboarding"
+              target="_blank"
+              onClick={() => va.track("qnaJoinBoom")}
+            >
+              Schedule with Boom
+            </Link>
+          </button>
+          <button className="buttonMobile md:button mt-2 md:mt-3 text-md md:text-xl 2xl:mt-16">
+            <Link
+              href="https://calendly.com/kailicen/kaikul-beta-onboarding"
+              target="_blank"
+              onClick={() => va.track("qnaJoinKaili")}
+            >
+              Schedule with Kaili
+            </Link>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { SocialIcon } from "react-social-icons";
+import Link from "next/link";
 
 type Props = {};
 
@@ -14,90 +15,105 @@ function Team({}: Props) {
       <div className="grid md:grid-cols-2 gap-5 px-2 md:px-10">
         <div
           className="bg-white rounded-lg shadow-md px-2 py-5 md:p-5 text-sm md:text-base
-        cardHover flex flex-col items-center"
+        cardHover "
         >
-          <div className="w-32 h-32 rounded-full overflow-hidden">
-            <Image
-              src="/img/boom.jpg"
-              alt="Boom's photo"
-              width={128}
-              height={128}
-            />
-          </div>
-          <h3 className="text-xl text-violet-800 font-bold mt-6">
-            Setthawut Kulsrisuwan (Boom)
-          </h3>
-          <p className="text-center mt-2">
-            Hi, I&apos;m Boom - your admin for self-discovery sessions and
-            community interaction. My mission is to create objective-setting and
-            self-awareness sessions, promote collaboration among diverse
-            individuals, and foster strong connections within our community. Are
-            you ready? Let&apos;s progress together towards a better future!
-          </p>
-          <div className="flex flex-row space-x-2 mt-2">
-            <SocialIcon
-              url="https://www.linkedin.com/in/setthawutkul/"
-              target="_blank"
-              bgColor="#5b21b6"
-              className="mt-2"
-            />
-            <SocialIcon
-              url="https://setthawut-kul.medium.com/startup-journey-1st-month-fbf6af794a8c"
-              target="_blank"
-              bgColor="#5b21b6"
-              className="mt-2"
-            />
-            <SocialIcon
-              url="https://setthawut.mystrikingly.com/"
-              target="_blank"
-              bgColor="#5b21b6"
-              className="mt-2"
-            />
-          </div>
+          <Link
+            className="flex flex-col items-center"
+            href="https://www.linkedin.com/in/setthawutkul/"
+            target="_blank"
+          >
+            <div className="w-32 h-32 rounded-full overflow-hidden">
+              <Image
+                src="/img/boom.jpg"
+                alt="Boom's photo"
+                width={128}
+                height={128}
+              />
+            </div>
+            <h3 className="text-xl text-violet-800 font-bold mt-6">
+              Setthawut Kulsrisuwan (Boom)
+            </h3>
+            <p className="text-center mt-2">
+              Hi, I&apos;m Boom - your admin for self-discovery sessions and
+              community interaction. My mission is to create objective-setting
+              and self-awareness sessions, promote collaboration among diverse
+              individuals, and foster strong connections within our community.
+              Are you ready? Let&apos;s progress together towards a better
+              future!
+            </p>
+            <div className="flex flex-row space-x-2 mt-2">
+              <SocialIcon
+                url="https://www.linkedin.com/in/setthawutkul/"
+                target="_blank"
+                bgColor="#5b21b6"
+                className="mt-2"
+              />
+              <SocialIcon
+                url="https://setthawut-kul.medium.com/startup-journey-1st-month-fbf6af794a8c"
+                target="_blank"
+                bgColor="#5b21b6"
+                className="mt-2"
+              />
+              <SocialIcon
+                url="https://setthawut.mystrikingly.com/"
+                target="_blank"
+                bgColor="#5b21b6"
+                className="mt-2"
+              />
+            </div>
+          </Link>
         </div>
         <div
           className="bg-white rounded-lg shadow-md px-2 py-5 md:p-5 text-sm md:text-base
-        cardHover flex flex-col items-center"
+        cardHover"
         >
-          <div className="w-32 h-32 rounded-full overflow-hidden">
-            <Image
-              src="/img/kaili.jpg"
-              alt="Kaili's photo"
-              width={128}
-              height={128}
-            />
-          </div>
-          <h3 className="text-xl text-violet-800 font-bold mt-6">Kaili Cen</h3>
-          <p className="text-center mt-2">
-            Hey there, I&apos;m Kaili, based in Hobart, Australia. As a
-            technical business analyst for the Tasmanian Department of
-            Education, I spend my days juggling work with my passion project -
-            KaiKul. My own journey has been tough and lonely, which is why I
-            created KaiKul as a space where people can share their personal
-            growth journeys without holding back. It&apos;s all about supporting
-            each other as we grow spiritually, mentally, emotionally, and
-            physically. Join us!
-          </p>
-          <div className="flex flex-row space-x-2 mt-2">
-            <SocialIcon
-              url="https://www.linkedin.com/in/kaili-cen-1975b4197/"
-              target="_blank"
-              bgColor="#5b21b6"
-              className="mt-2"
-            />
-            <SocialIcon
-              url="https://medium.com/@kailicen226"
-              target="_blank"
-              bgColor="#5b21b6"
-              className="mt-2"
-            />
-            <SocialIcon
-              url="https://www.kailicen.com/"
-              target="_blank"
-              bgColor="#5b21b6"
-              className="mt-2"
-            />
-          </div>
+          <Link
+            className="flex flex-col items-center"
+            href="https://www.linkedin.com/in/kaili-cen-1975b4197/"
+            target="_blank"
+          >
+            <div className="w-32 h-32 rounded-full overflow-hidden">
+              <Image
+                src="/img/kaili.jpg"
+                alt="Kaili's photo"
+                width={128}
+                height={128}
+              />
+            </div>
+            <h3 className="text-xl text-violet-800 font-bold mt-6">
+              Kaili Cen
+            </h3>
+            <p className="text-center mt-2">
+              Hey there, I&apos;m Kaili, based in Hobart, Australia. As a
+              technical business analyst for the Tasmanian Department of
+              Education, I spend my days juggling work with my passion project -
+              KaiKul. My own journey has been tough and lonely, which is why I
+              created KaiKul as a space where people can share their personal
+              growth journeys without holding back. It&apos;s all about
+              supporting each other as we grow spiritually, mentally,
+              emotionally, and physically. Join us!
+            </p>
+            <div className="flex flex-row space-x-2 mt-2">
+              <SocialIcon
+                url="https://www.linkedin.com/in/kaili-cen-1975b4197/"
+                target="_blank"
+                bgColor="#5b21b6"
+                className="mt-2"
+              />
+              <SocialIcon
+                url="https://medium.com/@kailicen226"
+                target="_blank"
+                bgColor="#5b21b6"
+                className="mt-2"
+              />
+              <SocialIcon
+                url="https://www.kailicen.com/"
+                target="_blank"
+                bgColor="#5b21b6"
+                className="mt-2"
+              />
+            </div>
+          </Link>
         </div>
       </div>
     </div>
