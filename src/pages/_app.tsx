620764155a7e6239358dd4e80c7cadbd3ba72a93
kailicen <kailicen226@gmail.com>
2023-06-12 10:24:16 +1000

    hotjar issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
+import Hotjar from "@hotjar/browser";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-import Hotjar from "@hotjar/browser";
 
 const siteId = 3505619;
 const hotjarVersion = 6;
