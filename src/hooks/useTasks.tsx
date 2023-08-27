460217066b01aef4e093e46354100be3f6eccaa7
kailicen <kailicen226@gmail.com>
2023-08-27 23:58:48 +1000

    debug 2

--- src/hooks/useTasks.tsx
+++ src/hooks/useTasks.tsx
@@ -136,7 +136,7 @@ export const useTasks = (date: string, user: User) => {
           tasksForDay.push(task);
         });
         setTasks(tasksForDay);
-        console.log(tasks);
+        console.log(tasksForDay);
       },
 
       (error) => {
